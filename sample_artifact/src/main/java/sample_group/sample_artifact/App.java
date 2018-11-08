92190ce72a572c12f51ee675838aec1736f5eac3
VishwasDaundkar <44867005+VishwasDaundkar@users.noreply.github.com>
2018-11-08 17:15:34 +0530

    Update App.java
    This has been edited by another parallel developer.

--- sample_artifact/src/main/java/sample_group/sample_artifact/App.java
+++ sample_artifact/src/main/java/sample_group/sample_artifact/App.java
@@ -10,5 +10,7 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("Lets start fucking world...");
+        System.out.println("Ohhhh.....Yes....!!!");
+        
     }
 }
